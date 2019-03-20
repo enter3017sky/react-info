945de4a2ba0af74cc64734c56b3f8f5acee54678
enter3017sky <enter3017sky@gmail.com>
2019-03-20 22:59:05 +0800

    update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faIgloo } from '@fortawesome/free-solid-svg-icons'
-
 import Navbar from '../Navbar/Navbar'
 import Container from '../Container/Container'
 import Footer from '../Footer/Footer'
-
-import { HashRouter as Router, Route } from "react-router-dom";
-
-// library.add(faIgloo)
-
 
 class App extends Component {
     render() {
